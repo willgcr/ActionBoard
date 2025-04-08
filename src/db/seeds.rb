e5b150d6c3787515c758d27d9dc4e4b158bf9ae9
Willian Rocha <hello@willgcr.me>
2025-04-07 22:59:55 -0300

    Add seeding

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts 'Seeding database...'
+
+users = {}
+users['willian@penyapp.com'] = User.find_or_create_by!(email: 'willian@penyapp.com') { |u| u.password = '123456' }
+users['lio@penyapp.com']     = User.find_or_create_by!(email: 'lio@penyapp.com')     { |u| u.password = '123456' }
+users['casey@penyapp.com']   = User.find_or_create_by!(email: 'casey@penyapp.com')   { |u| u.password = '123456' }
+users['aaron@penyapp.com']   = User.find_or_create_by!(email: 'aaron@penyapp.com')   { |u| u.password = '123456' }
+
+puts 'Creating lists...'
+lists = []
+users.each do |email, user|
+  3.times do
+    lists << List.create!(
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.sentence,
+      owner_user: user
+    )
+  end
+end
+
+puts 'Sharing lists...'
+lists.sample(5).each do |list|
+  User.where.not(id: list.owner_user_id).sample(2).each do |user|
+    ListShare.find_or_create_by!(list: list, user: user)
+  end
+end
+
+puts 'Creating tasks...'
+lists.each do |list|
+  rand(5..10).times do
+    Task.create!(
+      title: Faker::Lorem.sentence(word_count: 4),
+      description: Faker::Lorem.paragraph,
+      list: list,
+      completed: [true, false].sample
+    )
+  end
+end
+
+puts 'Done!'
